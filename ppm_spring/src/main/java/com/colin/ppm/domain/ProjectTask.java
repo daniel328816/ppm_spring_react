b4557da0f736ea3238192ca365629e596c067684
jq1004 <junqingl@usc.edu>
2019-06-27 18:36:13 -0400

    *bug fix*- imporve backlog/task relation for delete

--- ppm_spring/src/main/java/com/colin/ppm/domain/ProjectTask.java
+++ ppm_spring/src/main/java/com/colin/ppm/domain/ProjectTask.java
@@ -23,7 +23,7 @@ public class ProjectTask {
     private Date create_At;
     private Date update_At;
     //ManyToOne with Backlog
-    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "backlog_id", updatable = false, nullable = false)
     @JsonIgnore
     private Backlog backlog;
