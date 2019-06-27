22a64fd9e39a2d4c52a4983f787247854641e0ca
jq1004 <junqingl@usc.edu>
2019-06-27 16:59:33 -0400

    find projectTask by projectsequencet

--- ppm_spring/src/main/java/com/colin/ppm/services/ProjectTaskService.java
+++ ppm_spring/src/main/java/com/colin/ppm/services/ProjectTaskService.java
@@ -10,6 +10,8 @@ import com.colin.ppm.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import static sun.misc.Version.print;
+
 @Service
 public class ProjectTaskService {
 
@@ -62,6 +64,29 @@ public class ProjectTaskService {
             throw new ProjectNotFoundException("Project with ID: '"+ id+"' does not exist");
         }
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
+    }
+
+    public ProjectTask findPTByProjectSequence(String backlog_id,String pt_id){
+        // make sure we are searching on an existing backlog
+        Backlog backlog = backlogRepository.findByProjectIdentifier(backlog_id);
+        if(backlog == null){
+            throw new ProjectNotFoundException("Project with ID: '"+backlog_id+"' does not exist");
+        }
+
+        // make sure that our task exists
+        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+
+        if(projectTask == null){
+            throw new ProjectNotFoundException("Project Task: '"+pt_id+"' not found ");
+        }
+
+        // make sure that the backlog/project id in the path corresponds to the right project
+        if(!projectTask.getProjectIdentifier().equals(backlog_id)){
+            throw new ProjectNotFoundException("Project Task: '"+pt_id+"' does not exist in project: '"+backlog_id);
+        }
+
+
+        return projectTask;
     }
 
 }
